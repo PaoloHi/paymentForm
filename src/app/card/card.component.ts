a9dabea2c7d2cd1b3432d5b78a3ba9dfa520aafc
paolofernandofloresrivera <paolo.flores@paymind.com>
2024-04-21 22:03:20 -0600

    form

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { CardService } from '../card.service';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent {
+
+  cardService = inject(CardService)
+
+  applyForm = new FormGroup({
+    cardHolderName: new FormControl(''),
+    cardNumber: new FormControl(''),
+    expirationMonth: new FormControl(''),
+    expirationYear: new FormControl(''),
+    cvv : new FormControl('')
+  });
+
+
+
+  submitApplication() {
+    this.cardService.submitApplication(
+      this.applyForm.value.cardHolderName ?? '',
+      this.applyForm.value.cardNumber ?? '',
+      this.applyForm.value.expirationMonth ?? '',
+      this.applyForm.value.expirationYear ?? '',
+      this.applyForm.value.cvv ?? ''
+    );
+  }
 
 }
